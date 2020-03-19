79fd90bad12419fc80f3f6f980d421768883c5d3
vintej <vinayaktj@gmail.com>
2020-03-19 12:53:32 +0100

    Update files and setup_lasp

--- setup_lasp.py
+++ setup_lasp.py
@@ -14,20 +14,8 @@ def start(child, ip, node):
     child.sendline('rebar3 shell --name '+node+'@'+ip)
     child.expect("Application lasp started on node '"+node+"@"+ip+"'")
     child.sendline("")
-    index = child.expect([node+'@'+ip, pexpect.EOF, pexpect.TIMEOUT])
-    if index == 0:
-        print(index)
-    elif index == 1:
-        print(index)
-    elif index == 2:
-        print(index)
-    elif index == 3:
-        print(index)
     def exp():
-        child.expect("("+node+"@"+ip+")")
-    child.sendline("")
-    exp()
-    child.sendline("")
+        child.expect(node+'@'+ip)
     exp()
     child.sendline("erlang:set_cookie(node(),'RPJVCXYDYULBNZFEFPHJ').")
     exp()
