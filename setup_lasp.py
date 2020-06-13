56851ce4f96731a5588596e3af1ea22b02877c25
vintej <vinayaktj@gmail.com>
2020-06-13 14:36:51 +0000

    BugDebugLowerHop8

--- setup_lasp.py
+++ setup_lasp.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
 
 import pexpect
-import subprocess
-import sys
+import os
 
-def start(child, ip, node):
+def update_container():
     c_prompt = 'root@'
-    #child = pexpect.spawn ('docker exec -it '+cont+' /bin/bash')
-    #child.logfile = sys.stdout
+    child = pexpect.spawn ('docker exec -it mn.d3 /bin/bash')
+    child.logfile = sys.stdout
     child.expect (c_prompt)
     child.sendline('cd /opt/lasp')
     child.expect(c_prompt)
-    child.sendline('rebar3 shell --name '+node+'@'+ip)
-    child.expect("Application lasp started on node '"+node+"@"+ip+"'")
-    child.sendline("")
-    def exp():
-        child.expect(node+'@'+ip)
-    exp()
-    child.sendline("erlang:set_cookie(node(),'RPJVCXYDYULBNZFEFPHJ').")
-    exp()
-    #return child
+    child.sendline('pkill -f rebar3')
+    child.expect(c_prompt)
+    child.sendline('ps -aef | grep rebar3')
+    child.expect(c_prompt)
+    child.sendline('git pull')
+    child.expect(c_prompt)
+    child.sendline('rm log/*')
+    child.expect(c_prompt)
+    child.sendline('echo "" > log/error.log')
+    child.expect(c_prompt)
+    child.sendline('echo "" > log/console.log')
+    child.expect(c_prompt)
+    child.sendline('echo "" > log/crash.log')
+    child.expect(c_prompt)
+    child.sendline("exit")
+    child.expect('root@csst-06:')
+    child.close()
+
+update_container()
+os.system('docker commit mn.d3 vinayaktj/lasp:dev')
+os.system('docker push vinayaktj/lasp:dev')
